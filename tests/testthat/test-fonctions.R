aa8fbfc7f0ef1c99dcfccc46dcc88b5ce68970aa
Felicianedel <felicia.nedel@gmail.com>
2026-04-16 10:28:38 +0200

    modification des tests

--- tests/testthat/test-fonctions.R
+++ tests/testthat/test-fonctions.R
@@ -3,16 +3,16 @@ test_that("multiplication works", {
 })
 
 test_that("filtre_genre fonctionne", {
-  res <- filter_gender(netflix, "Female")
+  res <- filtre_genre(netflix, "Female")
   expect_true(all(res$gender == "Female"))
 })
 
 test_that("temps_moyen_abonnement retourne un dataframe", {
-  res <- mean_watch_time_by_subscription(netflix)
+  res <- temps_moyen_abonnement(netflix)
   expect_s3_class(res, "data.frame")
 })
 
 test_that("plot retourne un ggplot", {
-  res <- plot_watch_time_by_subscription(netflix)
+  res <- temps_moyen_abonnement_graph(netflix)
   expect_s3_class(res, "ggplot")
 })
