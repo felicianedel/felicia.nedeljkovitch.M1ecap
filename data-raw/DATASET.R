ca7ac5c99fcbf685030bf1f243970d0b6c974a0b
Felicianedel <felicia.nedel@gmail.com>
2026-04-16 09:30:41 +0200

    Initialisation du package

---
+++ data-raw/DATASET.R
@@ -0,0 +1,7 @@
+#| eval: false
+## code to prepare `DATASET` dataset goes here
+url <- "https://www.kaggle.com/datasets/rhythmghai/netflix-user-watching-behavior-dataset"
+netflix <- read.csv("data/netflix_user_behavior_dataset.csv")
+
+netflix <- netflix[1:100, ]
+usethis::use_data(netflix, overwrite = TRUE)
